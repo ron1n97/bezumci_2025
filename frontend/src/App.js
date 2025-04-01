6b7f8e7345edf9070e6f8458358b322a4b13d7c2
ron1n97 <roman.guskov97@gmail.com>
2025-04-01 13:28:46 +0400

    feat: add base interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
+import backgroundImg from "./phone.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      backgroundImage:`url(${backgroundImg})`,
+    }}
+    >
+      <div style={{
+        fontSize: "100px",
+        color: "yellow",
+        marginLeft: "253px",
+        textShadow: "0 0 5px blue, 0 0 5px blue, 0 0 5px blue, 0 0 5px blue",
+      }}><i><p>Заметки</p></i></div>
+      <div
+      style={{
+        display: "flex"
+      }}>
+        <textarea style={{
+        borderColor: "yellow",
+        borderWidth: 10,
+        width: "500px",
+        height: "700px",
+        marginLeft: "200px"
+      }}></textarea>
+      <button style={{
+      color: "white",
+      backgroundColor: "yellow",
+      fontSize: 70,
+      borderWidth: 10,
+      width: "200px",
+      height: "400px",
+      marginLeft: "200px",
+      textShadow: "0 0 5px yellow, 0 0 5px yellow, 0 0 5px yellow, 0 0 5px yellow",
+    }}>Принять</button>
+      </div>
     </div>
   );
 }
